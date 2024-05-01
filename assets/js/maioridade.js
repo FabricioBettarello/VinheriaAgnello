dd40637948c095189649f48ba51353fac4d1c60a
Eric Ito <84513383+EricYj15@users.noreply.github.com>
2024-04-30 23:16:25 -0300

    Update maioridade.js

--- assets/js/maioridade.js
+++ assets/js/maioridade.js
@@ -2,10 +2,8 @@ function verificarIdade() {
     var idade = prompt("Por favor, informe sua idade:");
 
     if (idade >= 18) {
-      // Redirecionar para a página inicial
       window.location = "../pages/home.html";
     } else {
-      // Redirecionar para o Google
       window.location.href = "//google.com";
     }
   }

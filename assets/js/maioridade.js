851766fabb835b0fc1f713ad645acf4bb9a9c1d4
Fabricio <100129385+FabricioBettarello@users.noreply.github.com>
2024-05-02 22:54:41 -0300

    Update maioridade.js

--- assets/js/maioridade.js
+++ assets/js/maioridade.js
@@ -2,9 +2,9 @@ function verificarIdade() {
     var idade = prompt("Por favor, informe sua idade:");
 
     if (idade >= 18) {
-      window.location = "../pages/home.html";
+      window.location = "https://fabriciobettarello.github.io/VinheriaAgnello/pages/home.html";
     } else {
-      window.location.href = "//google.com";
+      window.location.href = "https://google.com";
     }
   }
   window.unload = verificarIdade();
